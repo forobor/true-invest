5134d4a47625e4f84c1b35995044aab8ec139b8f
Dem <forobor@users.noreply.github.com>
2018-04-02 18:56:34 +0400

    (fix) app

--- client/src/App.js
+++ client/src/App.js
@@ -16,23 +16,7 @@ const AppWrapper = styled.div`
 
 class App extends Component {
 
-  state = {
-    response: ''
-  };
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/companies');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
 
 
 
